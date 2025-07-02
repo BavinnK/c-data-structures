47547422131bf68f0f4fa130fcb61185ab6126a8
Bavin <bavinbav5@gmail.com>
2025-07-03 00:05:09 +0300

    implemented 2 function one to delete first node second one to delete at any position

--- doubleLinkedList.c
+++ doubleLinkedList.c
@@ -110,6 +110,59 @@ struct node* delete_last_node(struct node* head){
   return head;
 
 }
+struct node* delete_first_node(struct node* head){
+  struct node* ptr1=NULL;
+  if(head==NULL){
+    printf("no data to delete!");
+    return head;
+  }
+  else if(head->next==NULL){
+    free(head);
+    head=NULL;
+    return head;
+  }
+  else{
+    ptr1=head;
+    head=head->next;
+    head->prev=NULL;
+    ptr1->next=NULL;
+    free(ptr1);
+    ptr1=NULL;
+    return head;
+  }
+
+}
+struct node* delete_node_position(struct node* head,int position){
+  struct node* ptr1=NULL; // i will try using one pointer instead doing with two i challenge mayself lol
+  if(head==NULL){
+    printf("no data to delete! ");
+    return head;
+  }
+  else if (position==1){
+    head=delete_first_node(head);
+    return head;
+  }
+  else{
+    ptr1=head;
+    while (position>1)
+    {
+      ptr1=ptr1->next;
+      position--;
+      
+    }
+    if(ptr1->next==NULL){
+        head=delete_last_node(head);
+        return head;
+      }
+    ptr1->prev->next=ptr1->next;
+    ptr1->next->prev=ptr1->prev;
+    free(ptr1);
+    ptr1=NULL;
+    return head;
+    
+
+  }
+}
 int main(void) {
   struct node*head=NULL;
   head=add_data_beg(head, 10);
@@ -121,12 +174,18 @@ int main(void) {
   head=add_data_end(head, 45);
   print(head);
   printf("\n");
-  reverse_print(head);
-  
+  //reverse_print(head);
+  #if 0
   head=delete_last_node(head);
   printf("\nafter deletion!\n");
   print(head);
   
-  
+  printf("\nafter deletion2!\n");
+  head=delete_first_node(head);
+  print(head);
+  # endif
+printf("\nafter deletion!\n");
+ head=delete_node_position(head,7);
+ print(head);
   return 0;
 }
