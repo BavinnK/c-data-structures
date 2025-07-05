da8662301a73a8d48fa31784721fbf1e03459e80
Bavin <bavinbav5@gmail.com>
2025-07-05 23:42:24 +0300

    implemented 3 more function which they are print function add at the end node function and also create list fun

--- CircularSinglyLinkedList.c
+++ CircularSinglyLinkedList.c
@@ -28,8 +28,78 @@ struct node* add_node_beg(struct node* tail,int data){
     }
 
 }
+void  print(struct node* tail)
+{
+    struct node* ptr1=NULL;
+    if (tail==NULL)
+    {
+        printf("no data to print!");
+        return;
+    }
+    else{
+        ptr1=tail->next;
+        do
+        {
+            printf("%d ",ptr1->data);
+            ptr1=ptr1->next;
+            
+        } while (ptr1!=tail->next);
+        
+        
+    }
+    
+
+}
+struct node* add_node_end(struct node* tail,int data){
+    
+    if (tail==NULL)
+    {
+        tail=addToEmpty(tail,data);
+        return tail;
+    }
+    struct node* ptr1=(struct node*)malloc(sizeof(struct node));
+    ptr1->data=data;
+    ptr1->next=NULL;
+    
+        ptr1->next=tail->next;
+        tail->next=ptr1;
+        tail=tail->next;
+        return tail;
+    
+    
+}
+struct node* create_list(struct node* tail){
+    int n,data;
+    printf("how many nodes you want to add: ");
+    scanf("%d",&n);
+    printf("\n enter number 1: ");
+    scanf("%d",&data);
+    tail=addToEmpty(tail,data);
+    for (int i = 1; i < n; i++)
+    {
+        printf("\n enter number %d: ",(1+i));
+        scanf("%d",&data);
+        tail=add_node_end(tail,data);
+        
+    }
+    return tail;
+    
+}
+
+
 
 int main(void){
     struct node* tail=NULL;
+    #if 0
+    tail = addToEmpty(tail, 10);
+    tail = add_node_beg(tail, 20);
+    tail = add_node_beg(tail, 30);
+    tail=add_node_end(tail,40);
+    tail=add_node_end(tail,100);
+    tail=add_node_end(tail,20);
+    print(tail);
+    #endif
+    tail=create_list(tail);
+    print(tail);
     return 0;
 }
