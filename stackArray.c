c8ead53ca21f8ce28a0c88cce390e146715e82ed
Bavin <bavinbav5@gmail.com>
2025-07-11 19:01:06 +0300

    implemented 4 kind of functions push-pop-isFULL-isEMPTY

---
+++ stackArray.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h> //this one is for the exit function in the pop function also in print
+#define MAX 5
+int stack_array[MAX];
+int top=-1;
+
+//this function checks if the array is overflow or not if its it returns 1 if not 0
+int isFULL(){
+    if (top==MAX-1)
+    {
+        return 1;
+    }
+    else{
+        return 0;
+    }
+    
+}
+//this function adds the data to the stack array
+void push(int data){
+    if (isFULL())
+    {
+        printf("stack overflow!");
+        return;
+    }
+    top++;
+    stack_array[top]=data;
+ 
+}
+//this function checks if the array is underflow or not if its it returns 1 if not 0
+int isEMPTY(){
+    if (top<0)
+    {
+        return 1;
+    }
+    else
+        return 0;
+    
+}
+//this function pops the top data in the array
+int pop(){
+    int result;
+    if (isEMPTY())
+    {
+        printf("stack underflow!");
+        exit(1);
+    }
+    result=stack_array[top];
+    top--;
+    return result;
+    
+}
+void print_data(){
+    if (isEMPTY())
+    {
+        printf("there is no data to print!");
+        exit(1);
+    }
+    for (int i = top; i >=0; i--)
+    {
+        printf("%d ",stack_array[i]);
+    }
+    
+    
+}
+
+
+
+
+int main(void){
+    int data;
+    push(3);
+    push(7);
+    data=pop();
+    print_data();
+    
+    return 0;
+}
